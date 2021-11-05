fe20ea11819d468d95224687778b4286f8edd18c
Bálint Tóth <tothbalesz@gmail.com>
2021-11-05 19:58:34 +0100

    Saop Service Done
    Finish the Soap server and the clinet.

--- controllers/webserviceController.php
+++ controllers/webserviceController.php
@@ -4,33 +4,32 @@
         public function show(){
 
             try{
-            //require_once('soaproot.php');
-
-            ini_set("default_socket_timeout",5000);
-
+            
+            //ini_set("default_socket_timeout",5000);
             $options = array(
             "location" => "http://localhost/web2bead1/models/webservice.php",
             "uri" =>"http://localhost/web2bead1/models/webservice.php",
             'keep_alive' => false,
             'trace' =>true,
-            'connection_timeout' => 5000,
-            'cache_wsdl' => WSDL_CACHE_NONE,
+            //'connection_timeout' => 5000,
+            //'cache_wsdl' => WSDL_CACHE_NONE,
             );
 
             $kliens = new SoapClient(null, $options);
-            $vege = $kliens->proba();     
-        
+            $gepek = $kliens->getgep();  
+            $szoftverek = $kliens->getszoftver();
+            $telepitesek = $kliens->gettelepites();
+
             require_once('views/webservice/show.php');
+
             }
             catch(SoapFault $e){
                 print ($kliens->__getLastResponse());
+                print ($kliens->__getLastRequest());
             }
         }
 
-       /*  public function show(){
-            //$this->connect();
-            require_once('views/webservice/show.php');
-        } */
+      
      }
 
 
--- models/webservice.php
+++ models/webservice.php
@@ -1,10 +1,45 @@
 <?php 
-    //require_once('soaproot');
-
     class Webservice{
-        public function proba() {
-            return 'proba';
+        public function getgep() {
+            $result = array();
+            
+            $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+            $db = new PDO('mysql:host=localhost;dbname=web2bead1', 'root', '', $pdo_options);
+            $sql = 'SELECT id, hely, tipus, ipcim FROM gep ORDER BY id';
+            $req = $db->prepare($sql);
+            $req->execute(array());
+            $result = $req->fetchAll(PDO::FETCH_ASSOC);
+            
+            return $result;
         }
+
+        public function getszoftver() {
+            $result = array();
+
+            $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+            $db = new PDO('mysql:host=localhost;dbname=web2bead1', 'root', '', $pdo_options);
+            $sql = 'SELECT id, nev, kategoria  FROM szoftver ORDER BY id';
+            $req = $db->prepare($sql);
+            $req->execute(array());
+            $result = $req->fetchAll(PDO::FETCH_ASSOC);
+            
+            return $result;
+        }
+
+        public function gettelepites() {
+            $result = array();
+
+            $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+            $db = new PDO('mysql:host=localhost;dbname=web2bead1', 'root', '', $pdo_options);
+            $sql = 'SELECT id, gepid, szoftverid, verzio, datum  FROM telepites ORDER BY id';
+            $req = $db->prepare($sql);
+            $req->execute(array());
+            $result = $req->fetchAll(PDO::FETCH_ASSOC);
+            
+            return $result;
+
+        }
+
     }
 
     $options = array("uri" => "http://localhost/web2bead1/models/webservice.php");
