1b50c75e455aa9ef838081ee6ea01421d198da6c
Bálint Tóth <tothbalesz@gmail.com>
2021-11-08 21:14:01 +0100

    Exchange function.

--- controllers/exchangerateController.php
+++ controllers/exchangerateController.php
@@ -1,10 +1,7 @@
 <?php
     class ExchangerateController{
             public function show(){
-                $result = array();
-                $currencies = array();
-
-
+                
                 try {
                     $client = new SoapClient("http://www.mnb.hu/arfolyamok.asmx?WSDL");
 
@@ -13,8 +10,8 @@
 
                 } catch (SoapFault $e) {
                     $result = $e;
-                }
-
+                } 
+ 
                 $firstDate = $result['FirstDate'];
                 $lastDate = $result['LastDate'];
                 $currencies = $result['Currencies'];
@@ -22,5 +19,148 @@
                 require_once('views/exchangerate/show.php');
 
             }
+
+            public function exchange($firstCurrency, $secondCurrency, $date){
+                $this->date = $date;
+                $firstExchange = [
+                    'startDate' => $date,
+                    'endDate' => $date,
+                   'currencyNames' => $firstCurrency
+                ];
+
+                $secondExchange = [
+                    'startDate' => $date,
+                    'endDate' => $date,
+                  'currencyNames' => $secondCurrency
+                ];
+                
+                $checkfirstUnit = [
+                    'currencyNames' => $firstCurrency
+                ];
+
+                $checksecondUnit = [
+                    'currencyNames' => $secondCurrency
+                ];
+
+                try {
+                    $client = new SoapClient("http://www.mnb.hu/arfolyamok.asmx?WSDL");
+
+                    $firstExchangeResult = (array)simplexml_load_string($client->GetExchangeRates($firstExchange)->GetExchangeRatesResult);
+                    
+                } catch (SoapFault $e) {
+                    $firstExchangeResult = $e;
+                }
+
+                try {
+
+                    $secondExchangeResult = (array)simplexml_load_string($client->GetExchangeRates($secondExchange)->GetExchangeRatesResult);
+                    
+                } catch (SoapFault $e) {
+                    $secondExchangeResult = $e;
+                }
+
+                try {
+
+                    $checkfirstUnitResult = (array)simplexml_load_string($client->GetCurrencyUnits($checkfirstUnit)->GetCurrencyUnitsResult);
+                    
+                } catch (SoapFault $e) {
+                    $checkfirstUnitResult  = $e;
+                }
+
+                try {
+
+                    $checksecondUnitResult = (array)simplexml_load_string($client->GetCurrencyUnits($checksecondUnit)->GetCurrencyUnitsResult);
+                    
+                } catch (SoapFault $e) {
+                    $checksecondUnitResult  = $e;
+                }
+
+                // end($firstExchangeResult['Day']) NEED THE END() TO CUT THE ACTUAL VALUE(STRING)
+
+                
+//-> null days     
+                $firstrate=(float)end($firstExchangeResult['Day'])/(float)end($checkfirstUnitResult['Units']);
+                $secondtrate=(float)end($secondExchangeResult['Day'])/(float)end($checksecondUnitResult['Units']);
+
+                $exchangeRate = $firstrate/$secondtrate;
+                
+                
+                return $exchangeRate;
+            }
+
+            public function exchangeMonth($firstCurrency, $secondCurrency, $date){
+                $this->date = $date;
+
+                $dateSplitter = explode("-", $date);
+                $monthDayCal = cal_days_in_month(CAL_GREGORIAN, $dateSplitter['1'], $dateSplitter['0']);
+
+                $startDate = $dateSplitter['0']."-".$dateSplitter['1']."-01";
+                $endDate = $dateSplitter['0']."-".$dateSplitter['1']."-".$monthDayCal;
+
+                $firstExchange = [
+                    'startDate' => $startDate,
+                    'endDate' => $endDate,
+                   'currencyNames' => $firstCurrency
+                ];
+
+                $secondExchange = [
+                    'startDate' => $startDate,
+                    'endDate' => $endDate,
+                  'currencyNames' => $secondCurrency
+                ];
+                
+                $checkfirstUnit = [
+                    'currencyNames' => $firstCurrency
+                ];
+
+                $checksecondUnit = [
+                    'currencyNames' => $secondCurrency
+                ];
+
+                try {
+                    $client = new SoapClient("http://www.mnb.hu/arfolyamok.asmx?WSDL");
+
+                    $firstExchangeResult = (array)simplexml_load_string($client->GetExchangeRates($firstExchange)->GetExchangeRatesResult);
+                    
+                } catch (SoapFault $e) {
+                    $firstExchangeResult = $e;
+                }
+
+                try {
+
+                    $secondExchangeResult = (array)simplexml_load_string($client->GetExchangeRates($secondExchange)->GetExchangeRatesResult);
+                    
+                } catch (SoapFault $e) {
+                    $secondExchangeResult = $e;
+                }
+
+                try {
+
+                    $checkfirstUnitResult = (array)simplexml_load_string($client->GetCurrencyUnits($checkfirstUnit)->GetCurrencyUnitsResult);
+                    
+                } catch (SoapFault $e) {
+                    $checkfirstUnitResult  = $e;
+                }
+
+                try {
+
+                    $checksecondUnitResult = (array)simplexml_load_string($client->GetCurrencyUnits($checksecondUnit)->GetCurrencyUnitsResult);
+                    
+                } catch (SoapFault $e) {
+                    $checksecondUnitResult  = $e;
+                }
+
+                // end($firstExchangeResult['Day']) NEED THE END() TO CUT THE ACTUAL VALUE(STRING)
+
+                //$firstrate=(float)end($firstExchangeResult['Day'])/(float)end($checkfirstUnitResult['Units']);
+                //$secondtrate=(float)end($secondExchangeResult['Day'])/(float)end($checksecondUnitResult['Units']);
+
+                //$exchangeRate = $firstrate/$secondtrate;
+                
+                $dates=[$startDate, $endDate];
+                return $firstExchangeResult;
+            }
+
+        
     }
 ?>
--- views/exchangerate/show.php
+++ views/exchangerate/show.php
@@ -1,38 +1,82 @@
+
+<div class='box'>
+    <form class="field" name=Currency method="POST" action="">
+        <label class='label'>
+            Choose the currencys you want to exchange:
+        </label>
+        <select name='firstcurr'>
+            <option value="">
+                Choose...
+            </option>
+            <?php
+                foreach($currencies as $currencie){
+                    echo "<option value='$currencie'> ".$currencie. "</option>";
+                }
+            ?>
+        </select>
+        -
+        <select  name='secondcurr'>
+            <option value="">
+                Choose...
+            </option>
+            <?php
+                foreach($currencies as $currencie){
+                    echo "<option value='$currencie'> ".$currencie. "</option>";
+                }
+            ?>
+        </select>
+
+        <br>
+        <br>
+
+        <label class='label'>
+            Choose the date:
+        </label>
+
+        <input type="date" id="date" name="date" value="2021-01-01" >
+
+        <br>
+        <br>
+
+        <label class='label'>
+            If you want to see the whole month check the box:
+        </label>
+        <input type='checkbox' id='month' name='month'>
+        <br>
+        <br>
+        <input class='button' type="submit" value="Submit" name=exchangesubmit>
+    </form>
+
+</div>
+
 <?php 
-    //var_dump($firstDate);
-    //var_dump($lastDate);
-    
+
+    if(isset($_POST['firstcurr']) && isset($_POST['secondcurr']) && isset($_POST['date']) && $_POST['firstcurr']!='' 
+        && $_POST['secondcurr']!='' && ($_POST['date']>=$firstDate && $_POST['date']<=$lastDate)){
+
+        if(isset($_POST['month'])){
+            $date = $_POST['date'];
+            $firstcurr = $_POST['firstcurr'];
+            $secondcurr = $_POST['secondcurr'];
+
+            $data = $this->exchangeMonth($firstcurr, $secondcurr, $date);
+            var_dump($data);
+        }
+        else{
+            $date = $_POST['date'];
+            $firstcurr = $_POST['firstcurr'];
+            $secondcurr = $_POST['secondcurr'];  
+            
+            
+            $data = $this->exchange($firstcurr, $secondcurr, $date);
+            
+            echo "<div class='box'>
+                    <p>
+                        In ".$_POST['date']." <strong>1</strong> ".$_POST['firstcurr']." was ".number_format($data, 9, '.')." ".$_POST['secondcurr']."
+                    </p>
+                </div>";
+        }        
+    }else{
+        echo "Please give correct input!";
+    }
 ?>
-<select>
-    <option value="">
-        Choose...
-    </option>
-    <?php
-        foreach($currencies as $currencie){
-            echo "<option value=''> ".$currencie. "</option>";
-        }
-    ?>
-</select>
-
-<select>
-    <option value="">
-        Choose...
-    </option>
-    <?php
-        foreach($currencies as $currencie){
-            echo "<option value=''> ".$currencie. "</option>";
-        }
-    ?>
-</select>
-
-<label for="start">Start date:</label>
-
-<input type="date" id="start" name="trip-start"
-       value="2018-07-22"
-       min="2018-01-01" max="2018-12-31">
-
-<label for="last">Last date:</label>
-
-<input type="date" id="last" name="trip-last"
-       value="2018-07-22"
-       min="2018-01-01" max="2018-12-31">
